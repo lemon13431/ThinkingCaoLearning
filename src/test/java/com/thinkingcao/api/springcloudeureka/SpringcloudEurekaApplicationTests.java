2f8a92e059374f341e824e55f7de8827f4656e3f
叶庭 言 <12004306+lemon1343@user.noreply.gitee.com>
2025-08-22 10:06:26 +0800

    Learn

---
+++ src/test/java/com/thinkingcao/api/springcloudeureka/SpringcloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkingcao.api.springcloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
